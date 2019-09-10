152103e1b225c0d75ac1cee3d11ea021c84ca287
Simon Berner <sibebzh@gmail.com>
2019-09-10 14:43:03 +0200

    Initialise the project

---
+++ src/main/java/simonberner/com/kotlin_everywhere/KotlinEverywhereApplication.java
@@ -0,0 +1,13 @@
+package simonberner.com.kotlin_everywhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinEverywhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotlinEverywhereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/simonberner/com/kotlin_everywhere/KotlinEverywhereApplicationTests.java
@@ -0,0 +1,16 @@
+package simonberner.com.kotlin_everywhere;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KotlinEverywhereApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
