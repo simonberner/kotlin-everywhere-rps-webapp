ddbef2005d03e2dbaf43dcb7b554180c9b2c0938
Simon Berner <sibebzh@gmail.com>
2019-09-10 15:08:57 +0200

    Add the IndexController

---
+++ src/main/java/simonberner/com/kotlin_everywhere/IndexController.java
@@ -0,0 +1,38 @@
+package simonberner.com.kotlin_everywhere;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+@Controller
+public class IndexController {
+    private GameRepository gameRepository;
+
+    public IndexController(@Autowired GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    @GetMapping
+    public ModelAndView index(Map<String, Object> model) {
+        model.put("entries", gameRepository.findAll());
+        model.put("computerScoreTotal", gameRepository.computerScoreTotal().orElse(0L));
+        model.put("userScoreTotal", gameRepository.userScoreTotal().orElse(0L));
+
+        return new ModelAndView("index", model); // because `index.mustache`
+    }
+
+    @PostMapping
+    public String play(@RequestParam String userChoice) {
+        // this is a mock implementation for now
+
+        RockPaperScissorsGame g = new RockPaperScissorsGame(null, userChoice, "Rock", 0L, 0L);
+        gameRepository.save(g);
+
+        return "redirect:/"; // redirect to `/`
+    }
+}
